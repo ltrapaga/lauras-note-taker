3057bdb52180ca41e91d4c65cfe2f945437a2faf
Laura Trapaga <trapaga9@gmail.com>
2023-01-13 18:25:07 -0500

    Add server.js get requests

--- server.js
+++ server.js
@@ -13,3 +13,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Add a static middleware for serving assets in the public folder
 app.use(express.static('public'));
+
+// GET HTML route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET HTML route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET API route for notes page
+app.get('/api/notes', (req, res) => {
+    res.json();
+});
+
+// Listen for connections
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+});
